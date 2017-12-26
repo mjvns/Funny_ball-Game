cd7e44b965cc04f24e7ebee08c2ae2c90b31160f
Madhukar Jaiswal <madhukar0206jaiswal@gmail.com>
2017-12-27 04:31:21 +0530

    Add files via upload

---
+++ Background.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour {
+
+    public GameObject ball;
+    public GameObject background;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (background.transform.position.x < ball.transform.position.x-10) {
+            background.transform.position = new Vector3(ball.transform.position.x, -10, background.transform.position.z);
+        }
+        if (background.transform.position.z < ball.transform.position.z-10) {
+            background.transform.position = new Vector3(background.transform.position.x, -10, ball.transform.position.z);
+        }
+	}
+}
---
+++ ScoreManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour {
+
+    public static ScoreManager instance;
+    public int score;
+    public int highScore;
+
+    void Awake(){
+        if (instance == null) {
+            instance = this;
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+        score = 0;
+        PlayerPrefs.SetInt("score", score);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (PlayerPrefs.HasKey("highscore"))
+        {
+            if (PlayerPrefs.GetInt("highscore") < score)
+            {
+                PlayerPrefs.SetInt("highscore", score);
+            }
+        }
+        else
+        {
+            PlayerPrefs.SetInt("highscore", score);
+        }
+        highScore = PlayerPrefs.GetInt("highscore");
+        PlayerPrefs.SetInt("score", score);
+    }
+
+    void IncrementScore() {
+        score += 1;
+    }
+
+    public void IncrementOnCollisionWithDiamond() {
+        score += 3;
+    }
+
+    public void StartScore() {
+        InvokeRepeating("IncrementScore", 0.2f, 1.0f);
+    }
+
+    public void EndScore() {
+        CancelInvoke("IncrementScore");
+        PlayerPrefs.SetInt("score", score);
+
+        if (PlayerPrefs.HasKey("highscore"))
+        {
+            if (PlayerPrefs.GetInt("highscore") < score)
+            {
+                PlayerPrefs.SetInt("highscore", score);
+            }
+        }
+        else {
+            PlayerPrefs.SetInt("highscore", score);
+        }
+    }
+}

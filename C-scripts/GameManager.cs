5680ed0e96891e6c5dd5da3acc78f23d0196b137
Madhukar Jaiswal <madhukar0206jaiswal@gmail.com>
2017-12-27 04:48:10 +0530

    Add files via upload

---
+++ C-scripts/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager instance;
+    public bool gameOver;
+
+    private void Awake(){
+        if (instance == null) {
+            instance = this;
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+        gameOver = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void GameStart() {
+        ScoreManager.instance.StartScore();
+        UiManager.instance.GameStart();
+        GameObject.Find("PlatformSpawner").GetComponent<Platformspawn>().StartPlatformSpawn();
+    }
+
+    public void GameOver() {
+        ScoreManager.instance.EndScore();
+        UiManager.instance.GameOver();
+        gameOver = true;
+    }
+}
---
+++ C-scripts/UiManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UiManager : MonoBehaviour {
+
+    public static UiManager instance;
+
+    public GameObject funnyballPanel;
+    public GameObject gameOverPanel;
+    public GameObject tapText;
+    public GameObject SwapleftPanel;
+    public GameObject ScorePanel;
+    public Text score;
+    public Text highScore1;
+    public Text highScore2;
+    public Text ActualScoreheading;
+    public Text actualScore;
+
+    void Awake(){
+        if (instance == null) {
+            instance = this;
+        }
+    }
+
+
+    // Use this for initialization
+    void Start () {
+        highScore1.text = "High Score: " + PlayerPrefs.GetInt("highscore").ToString();
+    }
+
+    public void GameStart() {
+        tapText.SetActive(false);
+        ScorePanel.SetActive(true);
+        funnyballPanel.GetComponent<Animator>().Play("panelUp");
+        SwapleftPanel.GetComponent<Animator>().Play("Swapleft");
+    }
+
+    public void GameOver() {
+        score.text = PlayerPrefs.GetInt("score").ToString();
+        highScore2.text = PlayerPrefs.GetInt("highscore").ToString();
+        gameOverPanel.SetActive(true);
+    }
+
+    public void Restart() {
+        SceneManager.LoadScene(0);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        actualScore.text = PlayerPrefs.GetInt("score").ToString();
+	}
+}
